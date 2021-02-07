f744a6c722735490476f42b068525db84f536190
Artem Komarov <artemk@wingspan.app>
2021-02-07 14:29:53 +0200

    Rearrange translations

--- source/constants.ts
+++ source/constants.ts
@@ -1,32 +1,40 @@
 export const TRANSLATIONS = {
-  UBIO: {
-    name: "Біблія в пер. Івана Огієнка, 1962",
-    code: 186
-  },
-  UMT: {
-    name: "Свята Біблія [Новий Заповіт]: Сучасною Мовою",
-    code: 204
-  },
   NRP: {
     name: "Новый Русский Перевод",
-    code: 143
-  },
-  RSP: {
-    name: "Новый Завет: Современный перевод",
-    code: 201
+    code: 143,
   },
   SYNO: {
     name: "Синодальный перевод",
-    code: 400
+    code: 400,
+  },
+  RSP: {
+    name: "Новый Завет: Современный перевод",
+    code: 201,
+  },
+  UBIO: {
+    name: "Біблія в пер. Івана Огієнка, 1962",
+    code: 186,
+  },
+  UMT: {
+    name: "Свята Біблія [Новий Заповіт]: Сучасною Мовою",
+    code: 204,
+  },
+  УТТ: {
+    name: "Переклад Р. Турконяка",
+    code: 1755,
+  },
+  NIV: {
+    name: "New International Version",
+    code: 111,
+  },
+  NASB2020: {
+    name: "New American Standard Bible - NASB",
+    code: 2692,
   },
   KJV: {
     name: "King James Version",
-    code: 1
+    code: 1,
   },
-  MSG: {
-    name: "The Message",
-    code: 97
-  }
 };
 
 export const DEFAUL_TRANSLATION = "UBIO";
